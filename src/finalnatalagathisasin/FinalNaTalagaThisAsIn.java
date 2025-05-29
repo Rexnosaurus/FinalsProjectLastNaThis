b7690be70e371eb0bb089e1a946a58efbbae714a
rexiepimentelMBP <rexiepimentelMBP@localhost>
2025-05-29 13:09:16 +0800

    Final Na!!!!!

---
+++ src/finalnatalagathisasin/FinalNaTalagaThisAsIn.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+package finalnatalagathisasin;
+
+
+public class FinalNaTalagaThisAsIn {
+
+    public static void main(String[] args) {
+        Main main = new Main();
+        main.setVisible(true);
+        main.setSize(1280, 720);
+        main.setLocation(190, 165);
+    }
+
+}
---
+++ src/finalnatalagathisasin/ShapesFormula.java
@@ -0,0 +1,104 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package finalnatalagathisasin;
+
+
+public class ShapesFormula {
+
+    // 2D Shape Formulas
+    public String triangle() {
+        return "<html>Perimeter = a + b + c<br>Area = (b * h) / 2</html>";
+    }
+
+    public String square() {
+        return "<html>Perimeter = 4 * a<br>Area = a^2</html>";
+    }
+
+    public String rectangle() {
+        return "<html>Perimeter = 2 * (l + w)<br>Area = l * w</html>";
+    }
+
+    public String parallelogram() {
+        return "<html>Perimeter = 2 * (a + b)<br>Area = b * h</html>";
+    }
+
+    public String rhombus() {
+        return "<html>Perimeter = 4 * a<br>Area = (d1 * d2) / 2</html>";
+    }
+
+    public String trapezoid() {
+        return "<html>Perimeter = a + b + c + d<br>Area = ((a + b) / 2) * h</html>";
+    }
+
+    public String pentagon() {
+        return "<html>Perimeter = 5 * a<br>Area = (5 * a^2) / (4 * tan(π/5))</html>";
+    }
+
+    public String hexagon() {
+        return "<html>Perimeter = 6 * a<br>Area = (3 * sqrt(3) * a^2) / 2</html>";
+    }
+
+    public String heptagon() {
+        return "<html>Perimeter = 7 * a<br>Area = (7 * a^2) / (4 * tan(π/7))</html>";
+    }
+
+    public String octagon() {
+        return "<html>Perimeter = 8 * a<br>Area = 2 * (1 + sqrt(2)) * a^2</html>";
+    }
+
+    public String nonagon() {
+        return "<html>Perimeter = 9 * a<br>Area = (9 * a^2) / (4 * tan(π/9))</html>";
+    }
+
+    public String decagon() {
+        return "<html>Perimeter = 10 * a<br>Area = (5 * a^2) / (2 * tan(π/10))</html>";
+    }
+
+    public String circle() {
+        return "<html>Circumference = 2 * π * r<br>Area = π * r^2</html>";
+    }
+
+    public String ellipse() {
+        return "<html>Perimeter ≈ 2 * π * sqrt((a^2 + b^2) / 2)"
+                + "<br>Area = π * a * b</html>";
+    }
+
+    // 3D Shape Formulas
+    public String cube() {
+        return "<html>Surface Area = 6 * a^2<br>Volume = a^3</html>";
+    }
+
+    public String rectangularPrism() {
+        return "<html>Surface Area = 2 * (l*w + w*h + h*l)<br>Volume = l * w * h</html>";
+    }
+
+    public String triangularPrism() {
+        return "<html>Surface Area = b*h + 2*(b*l) + 2*(s*l)<br>Volume = (b*h/2) * l</html>";
+    }
+
+    public String squarePyramid() {
+        return "<html>Surface Area = a^2 + 2 * a * slant height<br>Volume = (a^2 * h) / 3</html>";
+    }
+
+    public String cylinder() {
+        return "<html>Surface Area = 2 * π * r * (r + h)<br>Volume = π * r^2 * h</html>";
+    }
+
+    public String cone() {
+        return "<html>Surface Area = π * r * (r + slant height)<br>Volume = (π * r^2 * h) / 3</html>";
+    }
+
+    public String sphere() {
+        return "<html>Surface Area = 4 * π * r^2<br>Volume = (4/3) * π * r^3</html>";
+    }
+
+    public String hemisphere() {
+        return "<html>Surface Area = 3 * π * r^2<br>Volume = (2/3) * π * r^3</html>";
+    }
+
+}
+
+
